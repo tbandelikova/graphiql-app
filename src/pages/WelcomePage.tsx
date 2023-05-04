e6610853eaf1b7078716452dda031ba2b347bbdc
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-05-04 20:46:23 +0300

    feat: update project build

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import Counter from '../components/Counter/Counter';
-import TodosList from '../components/TodosList/TodosList';
 
 const WelcomePage = () => {
-  return (
-    <div>
-      <Counter />
-      <hr />
-      <TodosList />
-    </div>
-  );
+  return <div>Welcome Page</div>;
 };
 
 export default WelcomePage;
