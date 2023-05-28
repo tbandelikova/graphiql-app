72ae7ef39af3995f27d38097aee9f96e9faa004e
tat.bandelikova <tat.bandelikova@gmail.com>
2023-05-29 00:20:19 +0300

    fix: form fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/graphiql-app/',
   plugins: [react()],
   build: {
     rollupOptions: {
